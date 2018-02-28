25ed3f1db0eba44666bd8c1e8db4e345cada5a9b
JonSn0w <bstevensondev@gmail.com>
2018-02-27 18:02:38 -0800

    :rocket: Improve command-line argument handling

--- UDTherapy.py
+++ UDTherapy.py
@@ -10,73 +10,33 @@
 # functioning member of society.
 #*****************************************************************
 
-import sys, html, urllib.request, re, textwrap
+import sys, html, re, textwrap
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
+from urllib import request
 from random import randint
 
 color = { 'PURPLE':'\033[95m', 'CYAN':'\033[96m', 'DARKCYAN':'\033[36m', 'BLUE':'\033[94m', 'GREEN':'\033[92m', 'YELLOW':'\033[93m', 'RED':'\033[91m', 'BOLD':'\033[1m', 'UNDERLINE':'\033[4m', 'END':'\033[0m' }
 
-def form(s):
-  if "win" in sys.platform:
-    s[0] = '\nWord: '+str(s[0])
-    s[1] = 'Def: '+str(s[1])
-    s[2] = 'Ex: '+str(s[2])
-    return '\n'.join(s)
-  elif "linux" or "darwin" in sys.platform:
-    # need to test compatability on OS X
-    s[0] = color['PURPLE']+' \nWord: '+color['END']+s[0]
-    s[1] = color['YELLOW']+' Def: '+color['END']+s[1]
-    s[2] = color['DARKCYAN']+' Ex: '+color['END']+s[2]
-    return '\n'.join(s)
-
-def clean(s):
-  s = '\n      '.join(s.split('<br/><br/>'))
-  s = re.sub(re.compile('<.*?>'), '', s)
-  s = html.unescape(html.unescape(s))
-  if '\n' not in s:
-    s = '\n      '.join(textwrap.wrap(s, 60, break_long_words=False))
-  if '\"\"' in s:
-    s = '\"'.join(s.split('\"\"'))
-  return s
-
-def scrape(url, index):
-  page = urllib.request.urlopen(urllib.request.Request(url))
-  soup = BeautifulSoup(page, "html.parser")
-  term = []
-  if index >= len(re.findall(r'<a class="word" href=\"(.*?)\">(.*?)</a>', str(soup.findAll('a')))):
-    return None
-  term.append(re.findall(r'<a class="word" href=\"(.*?)\">(.*?)</a>', str(soup.findAll('a')))[index][1].capitalize()) # Word
-  term.append(clean(re.compile(r'<div class=\"meaning\">(.*?)</div>', re.S).findall(str(soup.findAll('div')).replace('\n', ''))[index].split('<br/> <br/>')[-1]).capitalize()) # Definition
-  term.append(clean('\"'+re.compile(r'<div class=\"example\">(.*?)</div>', re.S).findall(str(soup.findAll('div')).replace('\n', ''))[index]+'\"')) # Example
-  return term
-
-
 def main():
-
-  parser = ArgumentParser(description='Process some integers.')
+  parser = ArgumentParser(description='A simple rehabilitation program for coping with those long frustrating days of programming')
   parser.add_argument('-s', '--search', help='Returns the definition for the provided term')
   parser.add_argument('-n', '--num', type=int, help='Returns (n) definitions for the provided term')
   parser.add_argument('-a', '--all', action='store_true', help='Returns all definitions for the provided term')
   parser.add_argument('-w', '--wotd', action='store_true', help='Returns the definition for the word of the day')
   args = parser.parse_args()
-  print(vars(args))
+
   url = "https://www.urbandictionary.com/"
   term = []
   if args.num:
     index = args.num
   else:
     index = 6
-  # if len(vars(args)) == 0:
-  #   term = form(scrape(url, randint(0,index)))
-  # else:
   if args.wotd:
     term.append(form(scrape(url, 0)))
   elif args.search:
-    # if len(vars(args)) == 2:
     url += 'define.php?term='+args.search
     if args.all or args.num:
-      # url += 'define.php?term='+args[-1]
       for i in range(0,index):
         data = scrape(url, i)
         if data != None:
@@ -96,7 +56,6 @@ def main():
           sys.exit()
       term = form(term)
   elif args.all or args.num:
-    term = []
     for i in range(0,index):
       term.append(form(scrape(url, i)))
   else: 
@@ -106,7 +65,45 @@ def main():
     print(line)
   print('')
   sys.exit()
+
+
+def scrape(url, index):
+  term = []
+  page = request.urlopen(request.Request(url))
+  soup = BeautifulSoup(page, "html.parser")
+  if index >= len(re.findall(r'<a class="word" href=\"(.*?)\">(.*?)</a>', str(soup.findAll('a')))):
+    return None
+  term.append(re.findall(r'<a class="word" href=\"(.*?)\">(.*?)</a>', str(soup.findAll('a')))[index][1].capitalize()) # Word
+  term.append(clean(re.compile(r'<div class=\"meaning\">(.*?)</div>', re.S).findall(str(soup.findAll('div')).replace('\n', ''))[index].split('<br/> <br/>')[-1]).capitalize()) # Definition
+  term.append(clean('\"'+re.compile(r'<div class=\"example\">(.*?)</div>', re.S).findall(str(soup.findAll('div')).replace('\n', ''))[index]+'\"')) # Example
+  return term
   
+
+""" Formats the program output """
+def form(term):
+  if "win" in sys.platform:
+    term[0] = '\nWord: '+str(term[0])
+    term[1] = 'Def: '+str(term[1])
+    term[2] = 'Ex: '+str(term[2])
+  elif "linux" or "darwin" in sys.platform:
+    # need to test compatability on macOS
+    term[0] = color['PURPLE']+' \nWord: '+color['END']+term[0]
+    term[1] = color['YELLOW']+' Def: '+color['END']+term[1]
+    term[2] = color['DARKCYAN']+' Ex: '+color['END']+term[2]
+  return '\n'.join(term)
+
+
+""" Removes remaining HTML tags from term information """
+def clean(term):
+  term = '\n      '.join(term.split('<br/><br/>'))
+  term = re.sub(re.compile('<.*?>'), '', term)
+  term = html.unescape(html.unescape(term))
+  if '\n' not in s:
+    term = '\n      '.join(textwrap.wrap(term, 60, break_long_words=False))
+  if '\"\"' in s:
+    term = '\"'.join(s.split('\"\"'))
+  return term
+
 
 if __name__ == '__main__':
   main()
