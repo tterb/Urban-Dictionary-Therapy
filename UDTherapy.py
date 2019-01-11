c3a2ac604e5b53044f27f5388a40885e2f5e260a
tterb <bstevensondev@gmail.com>
2019-01-10 21:41:13 -0800

    :recycle: Cleanup and refactor code

--- UDTherapy.py
+++ UDTherapy.py
@@ -1,71 +1,68 @@
 #!/usr/bin/env python3
 #!C:/Python36/python3
-# UDTherapy.py
 
-#*****************************************************************
-# Urban Dictionary Therapy is a simple rehabilitation program for
-# coping with those long frustrating days of programming. Utilizing
-# this program and the information generously donated by the online
-# community, you too can return to your work as a more successful,
-# functioning member of society.
-#*****************************************************************
+"""
+Urban Dictionary Therapy
+A simple rehabilitation program for coping with long days of programming.
+Utilizing this program and the information generously donated by the online
+community, you too can return to your work as a more successful, functioning
+member of society.
+UDTherapy.py
+Brett Stevenson (c) 2017
+"""
 
-import sys, html, re, textwrap
-from argparse import ArgumentParser
+import sys, html, re, textwrap, argparse 
 from bs4 import BeautifulSoup
 from urllib import request
-from random import randint
+from random import randrange
 from colorama import init, Fore, Style
 
 
-def main():
+def main(args):
   init()
-  parser = ArgumentParser(description='A simple rehabilitation program for coping with those long frustrating days of programming')
+  term, index = [], 6
+  args = parse_options(args)
+  url = generate_url(args)
+  if args.num and args.num < 6:
+    index = args.num
+  if args.wotd:
+    term.append(scrape_term(url, 0))
+  elif args.all or args.num:
+    [term.append(scrape_term(url, i)) for i in range(0, index)]
+  else:
+    term.append(scrape_term(url, randrange(index)))
+  if not len(term) and args.search:
+    print("I'm sorry, there is no data for the given term")
+    sys.exit()
+  [print(form(line)) for line in term]
+
+
+def parse_options(args):
+  parser = argparse.ArgumentParser(prog='Urban Dictionary Therapy', description='A simple rehabilitation program for coping with those long days', usage='%(prog)s [options]', add_help=True)
   parser.add_argument('-s', '--search', help='Returns the definition for the provided term')
   parser.add_argument('-n', '--num', type=int, help='Returns (n) definitions for the provided term')
   parser.add_argument('-a', '--all', action='store_true', help='Returns the first page of definitions for the provided term')
   parser.add_argument('-w', '--wotd', action='store_true', help='Returns the definition for the word of the day')
-  args = parser.parse_args()
+  return parser.parse_args(args)
 
-  term = []
-  index = 6
-  url = "https://www.urbandictionary.com/"
-  if args.num and args.num < 6:
-    index = args.num
-  if args.wotd:
-    term.append(scrape(url, 0))
-  elif args.search:
+
+""" Returns the target URL """
+def generate_url(args):
+  url = 'https://www.urbandictionary.com/'
+  if args.search:
     url += 'define.php?term='+args.search
-    if args.all or args.num:
-        for i in range(index):
-          data = scrape(url, i)
-          if data != None:
-            term.append(data)
-          else:
-            print("I'm sorry, there is no data for the given term")
-            sys.exit()
-    else:
-      term.append(scrape(url, randint(0,index)))
-      while len(term) == 0:
-        index = index//2
-        term.append(scrape(url, randint(0,index)))
-        if index == 0:
-          print("I'm sorry, there is no data for the given term")
-          sys.exit()
-  elif args.all or args.num:
-    [term.append(scrape(url, i)) for i in range(0,index)]
-  else:
-    term.append(scrape(url, randint(0,index)))
-  [print(form(line)) for line in term]
+  return url
 
 
-def scrape(url, index):
+""" Retrieves data for the term at the given index """
+def scrape_term(url, index):
   term = []
-  page = request.urlopen(request.Request(url))
-  soup = BeautifulSoup(page, "html.parser")
-  if index >= len(re.findall(r'<a class="word" href=\"(.*?)\">(.*?)</a>', str(soup.findAll('a')))):
+  soup = BeautifulSoup(request.urlopen(request.Request(url)), "html.parser")
+  if not len(results):
     return None
-  term.append(re.findall(r'<a class="word" href=\"(.*?)\">(.*?)</a>', str(soup.findAll('a')))[index][1].capitalize()) # Word
+  elif index >= len(results):
+    index = len(results)-1
+  term.append(results[index][1].capitalize()) # Word
   term.append(clean(re.compile(r'<div class=\"meaning\">(.*?)</div>', re.S).findall(str(soup.findAll('div')).replace('\n', ''))[index].split('<br/> <br/>')[-1]).capitalize()) # Definition
   term.append(clean('\"'+re.compile(r'<div class=\"example\">(.*?)</div>', re.S).findall(str(soup.findAll('div')).replace('\n', ''))[index]+'\"')) # Example
   return term
@@ -82,7 +79,7 @@ def form(term):
 
 """ Removes remaining HTML tags from term information """
 def clean(term):
-  term = '\n      '.join(term.split('<br/><br/>'))
+  term = '\n\t\t'.join(term.split('<br/><br/>'))
   term = re.sub(re.compile('<.*?>'), '', term)
   term = term.replace('&amp;apos', '\'')
   term = html.unescape(html.unescape(term))
@@ -94,4 +91,4 @@ def clean(term):
 
 
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1:])
